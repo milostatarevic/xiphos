9f8a7e67beed7473bdc8e39d2db06446670f8360
Milos Tatarevic <milos.tatarevic@gmail.com>
2018-04-14 07:56:56 -0700

    v0.2

--- src/game.h
+++ src/game.h
@@ -21,7 +21,7 @@
 
 #include <inttypes.h>
 
-#define VERSION                      "Xiphos 0.1.11"
+#define VERSION                      "Xiphos 0.2"
 #define AUTHOR                       "Milos Tatarevic"
 
 #ifdef _BMI2
