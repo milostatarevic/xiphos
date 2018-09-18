b9c557539d02fb0dfcf47ea43cd87fe0d8e106d4
Milos Tatarevic <milos.tatarevic@gmail.com>
2018-09-17 18:37:01 -0700

    use asymmetric pst
    tc=4+0.04, hash=16MB
    Score of Xiphos 0.3.26 SSE vs Xiphos 0.3.25 SSE: 805 - 657 - 1371  [0.526] 2833
    Elo difference: 18.17 +/- 9.18
    
    tc=20+0.2, hash=64MB
    Score of Xiphos 0.3.26 SSE vs Xiphos 0.3.25 SSE: 875 - 741 - 2052  [0.518] 3668
    Elo difference: 12.70 +/- 7.45

--- src/game.h
+++ src/game.h
@@ -21,7 +21,7 @@
 
 #include <inttypes.h>
 
-#define VERSION                      "Xiphos 0.3.25"
+#define VERSION                      "Xiphos 0.3.26"
 #define AUTHOR                       "Milos Tatarevic"
 
 #ifdef _BMI2
