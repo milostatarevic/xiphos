c4aaed3c69efa2bced1f9a73d7d937087bf7d086
Milos Tatarevic <milos.tatarevic@gmail.com>
2019-01-27 16:04:59 -0800

    tweak lmp
    tc=10+0.1, hash=32MB
    Score of Xiphos 0.4.20 SSE vs Xiphos 0.4.19 SSE: 3781 - 3572 - 11672  [0.505] 19025
    Elo difference: 3.82 +/- 3.06
    
    tc=40+0.4, hash=64MB
    Score of Xiphos 0.4.20 SSE vs Xiphos 0.4.19 SSE: 1124 - 1001 - 4149  [0.510] 6274
    Elo difference: 6.81 +/- 5.00

--- src/game.h
+++ src/game.h
@@ -21,7 +21,7 @@
 
 #include <inttypes.h>
 
-#define VERSION                      "Xiphos 0.4.19"
+#define VERSION                      "Xiphos 0.4.20"
 #define AUTHOR                       "Milos Tatarevic"
 
 #ifdef _BMI2
